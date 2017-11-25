de7c6ff4c30e96eb8a11ef7a32d8886568f3ecfc
ArieDZ_2 <arifinmnur24@gmail.com>
2017-11-25 13:54:44 +0700

    Final

--- app/src/main/java/com/arifinmnur/VegetaApp/OrderFragment.java
+++ app/src/main/java/com/arifinmnur/VegetaApp/OrderFragment.java
@@ -1,14 +1,21 @@
 package com.arifinmnur.VegetaApp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,38 +31,79 @@ public class OrderFragment extends Fragment {
     }
 
     ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter2;
     View rootView;
 
-    String[] listBuah={
-       "Anggur","Apel","Jeruk","Mangga","Pisang","StrawBerry"
+    String[] listBuah = {
+            "Anggur", "Apel", "Jeruk", "Mangga", "Pisang", "StrawBerry"
     };
 
-    String[] listSayuran={
-            "Bawang Putih","Cabe","Jagung","Wortel","Brokoli","Bayam"
+    String[] listSayuran = {
+            "Bawang Putih", "Cabe", "Jagung", "Wortel", "Brokoli", "Bayam"
     };
 
     ArrayList<String> buahArrayList;
     ArrayList<String> sayuranArrayList;
+    Spinner sItems;
+    Spinner sItems2;
+    TextView nama;
+    TextView berat;
+    TextView jenis;
+    TextView jenisKiriman;
+    TextView hargaTotaltv;
+
+    CardView cd;
+    CardView report;
+    TextInputEditText ti_nama;
+    TextInputEditText ti_berat;
+
+
+    RadioButton jenkirKilat;
+    RadioButton jenkirRegular;
+    RadioButton jenkirEkonomi;
+    String buahselected;
+    String sayuranSelected;
+
+    double HargajenkirKilat=5000,
+        HargaJenkirRegular=3000,
+        HargaJenkirEkonomi=1000;
+
+    double hargaTotal=0,
+            hargakilo=5000;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-         rootView = inflater.inflate(R.layout.fragment_order, container, false);
+        rootView = inflater.inflate(R.layout.fragment_order, container, false);
+        nama = (TextView) rootView.findViewById(R.id.rptNama);
+        jenis = (TextView) rootView.findViewById(R.id.rptJenisBuah);
+        berat = (TextView) rootView.findViewById(R.id.rptBerat);
+        jenisKiriman = (TextView) rootView.findViewById(R.id.rptJenisKiriman);
+        hargaTotaltv=(TextView) rootView.findViewById(R.id.rptHargaTotal);
+
+        jenkirKilat = (RadioButton) rootView.findViewById(R.id.jenkirKilat);
+        jenkirRegular = (RadioButton) rootView.findViewById(R.id.jenkirRegular);
+        jenkirEkonomi = (RadioButton) rootView.findViewById(R.id.jenkirEkonomi);
+
+        cd = (CardView) rootView.findViewById(R.id.orderLayout);
+        report = (CardView) rootView.findViewById(R.id.detailOrder);
+        ti_nama = (TextInputEditText) rootView.findViewById(R.id.txInputNama);
+        ti_berat = (TextInputEditText) rootView.findViewById(R.id.txInputBerat);
+
 
         buahArrayList = new ArrayList<String>();
-        for(int i=0;i<listBuah.length;i++) {
+        for (int i = 0; i < listBuah.length; i++) {
             buahArrayList.add(listBuah[i]);
         }
 
         sayuranArrayList = new ArrayList<String>();
-        for(int i=0;i<listSayuran.length;i++) {
+        for (int i = 0; i < listSayuran.length; i++) {
             sayuranArrayList.add(listSayuran[i]);
         }
 
 
-
-        ArrayList<String> spinnerArray =  new ArrayList<String>();
+        ArrayList<String> spinnerArray = new ArrayList<String>();
         spinnerArray.add("Pilih");
         spinnerArray.add("Buah");
         spinnerArray.add("Sayuran");
@@ -64,31 +112,28 @@ public class OrderFragment extends Fragment {
                 getActivity(), android.R.layout.simple_spinner_item, spinnerArray);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner sItems = (Spinner) rootView.findViewById(R.id.spinnerJenis);
+        sItems = (Spinner) rootView.findViewById(R.id.spinnerJenis);
         sItems.setAdapter(adapter);
 
-        ArrayList<String> spinnerArrayBuah =  new ArrayList<String>();
+        ArrayList<String> spinnerArrayBuah = new ArrayList<String>();
         spinnerArrayBuah.add("Pilih");
 
 
-
-
-        adapter = new ArrayAdapter<String>(
+        adapter2 = new ArrayAdapter<String>(
                 getActivity(), android.R.layout.simple_spinner_item, spinnerArrayBuah);
-        Spinner sItems2 = (Spinner) rootView.findViewById(R.id.spinnerJenisdrop);
-        sItems2.setAdapter(adapter);
+        sItems2 = (Spinner) rootView.findViewById(R.id.spinnerJenisdrop);
+        sItems2.setAdapter(adapter2);
 
         // Spinner click listener
         sItems.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
 
-                String selected = String.copyValueOf(parent.getItemAtPosition(position)
-                        .toString(). toCharArray());
-                if (selected.equalsIgnoreCase("buah")) {
+                buahselected = String.copyValueOf(parent.getItemAtPosition(position)
+                        .toString().toCharArray());
+                if (buahselected.equalsIgnoreCase("buah")) {
                     dynamicDropdown(buahArrayList);
-                }
-                else if (selected.equalsIgnoreCase("Sayuran")) {
+                } else if (buahselected.equalsIgnoreCase("Sayuran")) {
                     dynamicDropdown(sayuranArrayList);
                 }
             }
@@ -99,14 +144,89 @@ public class OrderFragment extends Fragment {
             }
         });
 
+        sItems2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+
+                sayuranSelected = String.copyValueOf(parent.getItemAtPosition(position)
+                        .toString().toCharArray());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        Button btOrder = (Button) rootView.findViewById(R.id.btOrder);
+        btOrder.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(), "Order telah diKirim", Toast.LENGTH_SHORT).show();
+
+                nama.setText(ti_nama.getText());
+                berat.setText(ti_berat.getText());
+                jenis.setText(buahselected + " = " + sayuranSelected);
+
+                int berat = Integer.parseInt(ti_berat.getText().toString());
+                hargaTotal=hargakilo*berat;
+
+                if (jenkirKilat.isChecked()) {
+                    jenisKiriman.setText(jenkirKilat.getText()+" - Rp."+HargajenkirKilat);
+                    hargaTotal+=HargajenkirKilat;
+                }
+                if (jenkirRegular.isChecked()) {
+                    jenisKiriman.setText(jenkirRegular.getText()+" - Rp."+HargaJenkirRegular);
+                    hargaTotal+=HargaJenkirRegular;
+                }
+                if (jenkirEkonomi.isChecked()) {
+                    jenisKiriman.setText(jenkirRegular.getText()+" - Rp."+HargaJenkirEkonomi);
+                    hargaTotal+=HargaJenkirEkonomi;
+                }
+
+                hargaTotaltv.setText(String.valueOf(hargaTotal));
+
+
+                cd.setVisibility(View.GONE);
+                report.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button btFinnish = (Button) rootView.findViewById(R.id.btFininsh);
+        btFinnish.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(), "Order telah diKirim", Toast.LENGTH_SHORT).show();
+
+                ti_nama.setText("");
+                ti_berat.setText("");
+                jenkirEkonomi.setChecked(false);
+                jenkirRegular.setChecked(false);
+                jenkirKilat.setChecked(false);
+                sItems.setSelection(0);
+                sItems2.setAdapter(adapter2);
+
+
+                CardView cd = (CardView) rootView.findViewById(R.id.orderLayout);
+                cd.setVisibility(View.VISIBLE);
+                CardView report = (CardView) rootView.findViewById(R.id.detailOrder);
+                report.setVisibility(View.GONE);
+            }
+        });
         return rootView;
     }
- public void dynamicDropdown(ArrayList<String> arrayList){
-     adapter = new ArrayAdapter<String>(
-             getActivity(), android.R.layout.simple_spinner_item, arrayList);
 
-     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-     Spinner sItems2 = (Spinner) rootView.findViewById(R.id.spinnerJenisdrop);
-     sItems2.setAdapter(adapter);
- }
+    public void dynamicDropdown(ArrayList<String> arrayList) {
+        adapter = new ArrayAdapter<String>(
+                getActivity(), android.R.layout.simple_spinner_item, arrayList);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems2 = (Spinner) rootView.findViewById(R.id.spinnerJenisdrop);
+        sItems2.setAdapter(adapter);
+    }
 }
